9a66c3b4d2547298684e4a0bdc424bbb469a1671
Tarun-1362 <105165686+Tarun-1362@users.noreply.github.com>
2022-10-25 23:51:11 +0530

    Add files via upload

---
+++ StarPrint2.java
@@ -0,0 +1,25 @@
+
+public class StarPrint2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=14;
+		for (int i = 0; i < n; i++) {
+			
+			
+			for (int j = 0; j < n; j++) {
+				if (i==n-1||i-j>=(n-1)/2||i+j>n+(n-1)/3)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+					
+				
+				
+			}
+			System.out.println();
+			
+		}
+
+	}
+
+}
